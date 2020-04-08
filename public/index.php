c5212cd902de4ff40389cd4e70ac18f30895e3c8
Emile Calixte <calixte.emile@gmail.com>
2020-04-08 15:09:24 +0200

    class Calculatrice

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
-Hello toto
+<?php
+require('../Calculatrice.php');
+require('../vendor/autoload.php');
+
+function d(...$values) {
+    foreach($values as $value) {
+        echo '<pre>';
+        var_dump($value);
+        echo '</pre>';
+    }
+}
+
+function dd(...$values) {
+    d(...$values);
+    die;
+}
+
+
+
+$calc = new Calculatrice();
+
+d($calc->setA(12)->setB(5)->add());
+d($calc->setA(76)->setB(3)->div());
+?>
