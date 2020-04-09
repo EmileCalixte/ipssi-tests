4c8c03373abd9c950d2c9f1b2559e0e6c76e9183
Emile Calixte <calixte.emile@gmail.com>
2020-04-09 22:36:45 +0200

    Create Better Setters In Contact Class
    Base Yii setters does not validate the attributes rules (for example max
    length defined in db). Those custom setters will validate attributes
    rules

--- models/Contact.php
+++ models/Contact.php
@@ -6,5 +6,18 @@ namespace app\models;
 
 class Contact extends databaseModels\Contact
 {
+    public function setFirstname(string $firstname): self
+    {
+        return $this;
+    }
 
+    public function setLastname(string $lastname): self
+    {
+        return $this;
+    }
+
+    public function setPhoneNumber($phoneNumber): self
+    {
+        return $this
+    }
 }
