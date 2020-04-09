ec0add07762151ca94e65e1eea44cfbc498469f3
Emile Calixte <calixte.emile@gmail.com>
2020-04-09 22:27:10 +0200

    test_db config

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
-// test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2_basic_tests';
+// test database! Important not to run tests on production or development databases // Of course ! But for know we don't care
+//$db['dsn'] = 'mysql:host=localhost;dbname=yii2_basic_tests';
 
 return $db;
