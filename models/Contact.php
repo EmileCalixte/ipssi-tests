a1b14d58efd90bffae3bd43725bc50bb6ba3396e
Emile Calixte <calixte.emile@gmail.com>
2020-04-09 17:51:10 +0200

    Create Contact Model

---
+++ models/Contact.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace app\models;
+
+
+class Contact extends databaseModels\Contact
+{
+
+}
---
+++ models/databaseModels/Contact.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models\databaseModels;
+
+use Yii;
+
+/**
+ * This is the model class for table "contact".
+ *
+ * @property int $id
+ * @property string $firstname
+ * @property string $lastname
+ * @property string|null $phone_number
+ */
+class Contact extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'contact';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['firstname', 'lastname'], 'required'],
+            [['firstname', 'lastname'], 'string', 'max' => 64],
+            [['phone_number'], 'string', 'max' => 16],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'firstname' => 'Firstname',
+            'lastname' => 'Lastname',
+            'phone_number' => 'Phone Number',
+        ];
+    }
+}
