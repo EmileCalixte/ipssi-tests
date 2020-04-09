4d8784750c9e54fa4bdc98a5c8c3e676d595c9f6
Emile Calixte <calixte.emile@gmail.com>
2020-04-09 17:59:25 +0200

    Create ContactTest File

---
+++ tests/unit/models/ContactTest.php
@@ -0,0 +1,28 @@
+<?php namespace models;
+
+use app\models\Contact;
+
+class ContactTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    /** @var Contact */
+    private $contact;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        
+    }
+}
