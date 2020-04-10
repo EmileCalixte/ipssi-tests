cabde145fa43f7154e34a4e4b54d5916b9a6ffcb
Emile Calixte <calixte.emile@gmail.com>
2020-04-10 10:13:14 +0200

    Extend __set Magic Method In ActiveRecord

--- components/ActiveRecord.php
+++ components/ActiveRecord.php
@@ -4,7 +4,27 @@
 namespace app\components;
 
 
+use app\models\exceptions\MyCustomException;
+use yii\db\BaseActiveRecord;
+
 class ActiveRecord extends \yii\db\ActiveRecord
 {
+    /**
+     * Extends {@see BaseActiveRecord::__set} to validate new value when set
+     * @param string $name
+     * @param mixed $value
+     * @throws MyCustomException if attribute value does not match rules
+     */
+    public function __set($name, $value)
+    {
+        $oldValue = $this->$name;
 
+        parent::__set($name, $value);
+
+        if(!$this->validate($name)) {
+            parent::__set($name, $oldValue);
+
+            throw new MyCustomException(implode(' - ', $this->errors[$name]));
+        }
+    }
 }
