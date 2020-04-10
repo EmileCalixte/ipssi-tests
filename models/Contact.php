c3113c979a1fd4aef828917e7298e702d58881f8
Emile Calixte <calixte.emile@gmail.com>
2020-04-10 10:42:38 +0200

    Add Email Rule In Contact Class

--- models/Contact.php
+++ models/Contact.php
@@ -4,6 +4,15 @@
 namespace app\models;
 
 
+use yii\validators\EmailValidator;
+
 class Contact extends databaseModels\Contact
 {
+    public function rules()
+    {
+        return array_merge(parent::rules(),
+        [
+           [['email'], 'email'], /** Email field must be a valid email. {@see EmailValidator} */
+        ]);
+    }
 }
